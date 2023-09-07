515080d0aa8bdefe7b9437292c1ed833d2bafefa
Karanvir Sidhu <karan.sidhu98@gmail.com>
2023-09-07 14:02:08 -0700

    Implement logic for fibonnaci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  const fibNum = [0, 1];
+  for (let index = 1; index < num; index++) {
+    fibNum.push(fibNum[index] + fibNum[index - 1]);
+  }
+  return fibNum[num];
 };
 
 // Do not edit below this line
