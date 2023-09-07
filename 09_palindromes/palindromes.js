91252f48b1f8063a699b52d9634b43cea344085f
Karanvir Sidhu <karan.sidhu98@gmail.com>
2023-09-07 13:54:32 -0700

    Implement logic for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const valueString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return valueString.split("").reverse().join("") == valueString;
 };
 
 // Do not edit below this line
